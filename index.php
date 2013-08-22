87926317ce2a822cd7cf49df531025afda2c7232
James Reatherford <jamesreatherford@gmail.com>
2013-08-22 09:55:15 -0500

    Created as a  netbeans project

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "Yeah.  I am totes gonna need to put stuff here.<br>";
+echo "It is gonna be awesome.  Can't wait!"
+
+?>
